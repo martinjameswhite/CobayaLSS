8a2333b0e9feea765668102a0eaddc968368cf20
Martin J White <mwhite@berkeley.edu>
2021-05-26 17:25:53 -0700

    Some useful instructions.

--- create_cobaya_env_nersc.sh
+++ create_cobaya_env_nersc.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 #
-# Need to run this from an interactive shell, not a Jupyter "shell"
-# since those don't have the PrgEnv installed.
+# This works best if run from an interactive shell, not a Jupyter "shell".
 #
 # I needed to use PrgEnv-gnu in order to get the Planck lensing
 # likelihood to compile.
 # 
+# Set up an empty environment for Cobaya (cloning the base ensures
+# mpi4py is properly included in its NERSC form).
+#
 conda create --name cobaya --clone base
 #
+# Switch to the environment.
 source activate cobaya
+#
 # Install some basic stuff
 conda install -c conda-forge pyfftw -y
 conda install healpy -y
-# Use the "Cobaya" version of CLASS (installed with cobaya-install below)
-###cd /global/cfs/cdirs/m68/mwhite/class/python/
-###python setup.py install
+#
 # Set up the environment for Jupyter.
 conda install ipykernel ipython jupyter # Should already be there.
-python -m ipykernel install --user --name cobaya --display-name Cobaya-env
+python3 -m ipykernel install --user --name cobaya --display-name Cobaya-env
 #
-python -m pip install cobaya  # --upgrade
+# Now install Cobaya
+python3 -m pip install cobaya  # --upgrade
 #
+# and any "cosmo" packages it wants
 cobaya-install cosmo -m /global/cscratch1/sd/mwhite/Cobaya/Packages
 #
 # Install velocileptors. 
 #python3 -m pip install git+https://github.com/sfschen/velocileptors
-# and Anzu
+# and Anzu if you want.
 #conda install -c conda-forge pyccl chaospy -y
 #python3 -m pip install -v git+https://github.com/kokron/anzu
 #
