5961491ffe16e42f915cb4992fa7269e1d2e0cc0
Joe DeRose <joe.derose13@gmail.com>
2021-06-23 11:52:49 -0700

    some AP stuff

--- theory_lss/RSD.py
+++ theory_lss/RSD.py
@@ -2,6 +2,7 @@ import numpy as np
 from cobaya.theory import Theory
 from cobaya.theories.cosmo import PowerSpectrumInterpolator
 from velocileptors.LPT.lpt_rsd_fftw import LPT_RSD
+from scipy.interpolate import interp1d
 
 
 class RSDCalculator(Theory):
@@ -12,33 +13,13 @@ class RSDCalculator(Theory):
     kmin: float
     kmax: float
     nk: int
-    zmin: float
-    zmax: float
-    nz: float
     ngauss: int
     nmax: int
-    z: Union[Sequence, np.ndarray]
+    kIR: float
+    fiducial_exp_geo_filename: str
 
     def initialize(self):
         """called from __init__ to initialize"""
-
-        if hasattr(self, 'z'):
-            if self.z == []:
-
-                if hasattr(self, 'zmin'):
-                    zmin = self.zmin
-
-                    try:
-                        zmax = self.zmax
-                        nz = self.nz
-                        self.z = np.linspace(zmin, zmax, nz)
-                    except Exception as e:
-                        print(e)
-                        raise(e)
-            self.z = np.array(self.z)
-
-        self.z = np.hstack([self.z, np.zeros(1)])
-        self.z = np.unique(self.z)
 
         self.k = np.logspace(np.log10(self.kmin),
                              np.log10(self.kmax),
@@ -48,6 +29,11 @@ class RSDCalculator(Theory):
         self.nus = nus[0:self.ngauss]
 
         self.nspec = 19
+
+        #fiducial distances/hubble params
+        fid_exp_geo = np.genfromtxt(self.fiducial_exp_geo_filename, names=True)
+        self.fid_hz_spline = interp1d(fid_exp_geo['z'], fid_exp_geo['h'])
+        self.fid_chi_spline = interp1d(fid_exp_geo['z'], fid_exp_geo['chi'])
 
 
     def initialize_with_provider(self, provider):
@@ -62,15 +48,21 @@ class RSDCalculator(Theory):
         Return dictionary of derived parameters or other quantities that are needed
         by this component and should be calculated by another theory class.
         """
-        return {'Pk_interpolator': {'k_max': 10,
-                                    'z': self.z,
-                                    'nonlinear': False},
-                'sigma8_z': {'z': self.z},
-                'fsigma8_z': {'z': self.z}}
+        return {}
 
     def must_provide(self, **requirements):
 
-        return {}
+        if 'pknu_basis_spectrum_grid' in requirements:
+            self.z = requirements['pknu_basis_spectrum_grid']['z']
+            return {'Pk_interpolator': {'k_max': 10,
+                                        'z': self.z,
+                                        'nonlinear': False},
+                    'sigma8_z': {'z': self.z},
+                    'fsigma8_z': {'z': self.z},
+                    'Hubble': {'z': self.z},
+                    'angular_diameter_distance':{'z': self.z},
+                    'H0': None}
+
 
     def get_can_provide(self):
 
@@ -78,26 +70,34 @@ class RSDCalculator(Theory):
 
     def calculate(self, state, want_derived=True, **params_values_dict):
 
-        state['pknu_basis_spectrum_grid'] = {}
-
         k = self.k
         nu = self.nu
 
         pk_lin_interp = self.provider.get_Pk_interpolator(nonlinear=False)
+
+        #linear growth rate
         sigma8_z = self.provider.get_sigma8_z(self.z)
         fsigma8 = self.provider.get_fsigma8(self.z)
         f = fsigma8 / sigma8_z
 
-        state['pknu_basis_spectrum_grid'] = {}
-        state['pknu_basis_spectrum_grid']['k'] = self.k
-        state['pknu_basis_spectrum_grid']['nu'] = self.nu
-        state['pknu_basis_spectrum_grid']['power'] = np.zeros((len(z), len(k), len(nu), self.nspec)
+        #AP
+        hub = self.provider.get_param('H0')
+        hz_fid = self.fid_hz_spline(self.z)
+        chi_fid = self.fid_chi_spline(self.z)
+        apar = hz_fid/(self.provider.get_Hubble(self.z) * 2997.925 / hub)
+        aperp = (self.provider.get_angular_diameter_distance(self.z) *
+                 (1 + self.z) * hub) / da_fid
+
+        state['pknu_basis_spectrum_k'] = self.k
+        state['pknu_basis_spectrum_nu'] = self.nu
+        state['pknu_basis_spectrum_grid']= np.zeros((len(z), len(k), len(nu),
+                                                     self.nspec)
 
         for i, z in enumerate(self.z):
             pk = pk_lin_interp.P(z, self.k)
             lpt = LPT_RSD(self.k, pk, kIR=self.kIR)
             lpt.make_pltable(f[i], nmax=self.nmax, kmin=self.kmin,
                              kmax=self.kmax, nk=self.nk,
-                             apar=1,aperp=1)
+                             apar=apar[i],aperp=aperp[i])
 
-            state['pknu_basis_spectrum_grid']['power'][i,...] = lpt.pknutable
+            state['pknu_basis_spectrum_grid'][i,...] = lpt.pknutable
