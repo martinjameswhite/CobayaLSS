f5ba33d69c04dfef1adb0c79bbfb0dd6e9048112
Martin J White <mwhite@berkeley.edu>
2021-06-22 22:06:02 -0700

    Switch to provider.

--- lss_likelihood/img_likelihood.py
+++ lss_likelihood/img_likelihood.py
@@ -1,26 +1,19 @@
 import numpy as np
-import predict_cl  as T       # T is for "theory".
-import cl_model    as M
 import sys
 import os
 
-from classy import Class
+import predict_cl as T # For theory.
 
-from scipy.interpolate import InterpolatedUnivariateSpline as Spline
-
+from cobaya.theory     import Theory
 from cobaya.likelihood import Likelihood
 
 
 class ClLikelihood(Likelihood):
     # From yaml file.
-    mname: str
     clsfn: str
     covfn: str
     wlafn: str
     wlxfn: str
-    dndzfn:str
-    ityp:  str
-    isamp: int
     acut:  int
     xcut:  int
     #
@@ -29,61 +22,26 @@ class ClLikelihood(Likelihood):
         # Load the data and invert the covariance matrix.
         self.loadData()
         self.cinv= np.linalg.inv(self.cov)
-        #
-        # Set up a fiducial cosmology and Class instance, then
-        # compute the distance and Hubble parameter at zeff for the
-        # "fiducial" cosmology.
-        hub = 0.677
-        cc  = Class()
-        #cc.set({'output':'mPk','P_k_max_h/Mpc':100.,'z_pk':'0,10',\
-        #        'A_s':2.11e-9, 'n_s':0.96824, 'h':0.6760,\
-        #        'N_ur':2.0328, 'N_ncdm':1,'m_ncdm':0.06,\
-        #        'tau_reio': 0.057, 'omega_b':0.022447, 'omega_cdm':0.11923})
-        #cc.set({'output':'mPk','P_k_max_h/Mpc':100.,'z_pk':'0,10',\
-        #        'A_s':2.11e-9, 'n_s':0.96824, 'h':hub,\
-        #        'N_ur':2.0328, 'N_ncdm':1,'m_ncdm':0.06,\
-        #        'z_reio': 7.0, 'omega_b':0.022, 'omega_cdm':0.119})
-        #
-        cc.set({'output':'mPk','P_k_max_h/Mpc':100.,'z_pk':'0,10',\
-                'non linear': 'halofit',\
-                'A_s':1.90e-9, 'n_s':0.96824, 'h':hub,\
-                'N_ur':2.0328, 'N_ncdm':1,'m_ncdm':0.06,\
-                'z_reio': 7.0, 'omega_b':0.022, 'omega_cdm':0.119})
-        cc.compute()
-        self.OmM  = cc.Omega0_m()
-        self.omh3 = 0.09633# For setting h if not otherwise given.
-        # Set up a model instance and use this to get z_eff.
-        self.model= M.Model(self.mname,self.dndz,cc)
-        self.zeff = self.model.aps.zeff
+    def get_requirements(self):
+        """What we need."""
+        req = {'pt_cell_mod': None,\
+               'b1': None,\
+               'b2': None,\
+               'alpha_a': None,\
+               'alpha_x': None,\
+               'SN': None,\
+               'smag': None,\
+               'omegam': None\
+              }
+        return(req)
     def logp(self,**params_values):
         """
         Given a dictionary of nuisance parameter values params_values
         return a log-likelihood.
         """
-        #H0_theory = self.provider.get_param("H0")
-        #cls = self.provider.get_Cl(ell_factor=True)
-        OmM  = params_values['Omega_m']
-        if OmM<=0: OmM=0.3
-        hub  = params_values.get('hub',(self.omh3/OmM)**0.3333)
-        sig8 = params_values['sig8']
-        b1   = params_values['b1']
-        b2   = params_values['b2']
-        bs   = params_values['bs']
-        bn   = params_values['bn']
-        alpA = params_values['alpha_a']
-        alpX = params_values['alpha_x']
-        sn   = params_values['lgSN'];  sn = 10.0**sn
-        smag = params_values['smag']
-        #
-        # Set the fast and slow parameters.
-        fast = [b1,b2,bs,bn,alpA,alpX,sn,smag]
-        slow = [OmM,hub,sig8]
-        #
-        # Get the "theory".
-        tt = self.model(fast,slow)
-        # Now compute chi^2.
-        thy  = self.observe(tt)
-        chi2 = np.dot(self.dd-thy,np.dot(self.cinv,self.dd-thy))
+        thy  = self.predict()
+        obs  = self.observe(thy)
+        chi2 = np.dot(self.dd-obs,np.dot(self.cinv,self.dd-obs))
         #
         return(-0.5*chi2)
         #
@@ -93,7 +51,6 @@ class ClLikelihood(Likelihood):
         self.cov = np.loadtxt(self.covfn)
         self.wla = np.loadtxt(self.wlafn)
         self.wlx = np.loadtxt(self.wlxfn)
-        self.dndz= np.loadtxt(self.dndzfn)
         # Now pack things and modify the covariance matrix to
         # "drop" some data points.
         self.xx = dd[:,0]
@@ -109,6 +66,27 @@ class ClLikelihood(Likelihood):
             self.cov[ :,ii] = 0
             self.cov[ii,ii] = 1e15
         #
+    def predict(self):
+        """Predicts the theory C_ell's."""
+        pp   = self.provider
+        modPT= pp.get_result('pt_cell_mod')
+        b1   = pp.get_param('b1')
+        b2   = pp.get_param('b2')
+        alpA = pp.get_param('alpha_a')
+        alpX = pp.get_param('alpha_x')
+        SN   = pp.get_param('SN')
+        smag = pp.get_param('smag')
+        #
+        bs,b3  = 0.0,0.0
+        biases = [b1,b2,bs,b3]
+        cterms = [alpA,alpX]
+        stoch  = [SN]
+        pars   = biases + cterms + stoch
+        #
+        ell,clgg,clgk = modPT(pars,smag,Lmax=1251)
+        tt = np.array([ell,clgg,clgk]).T
+        return(tt)
+        #
     def observe(self,tt):
         """Applies the window function and binning matrices."""
         lmax = self.wla.shape[1]
@@ -118,4 +96,51 @@ class ClLikelihood(Likelihood):
         obs2 = np.dot(self.wlx,np.interp(ells,tt[:,0],tt[:,2],right=0))
         obs  = np.concatenate([obs1,obs2])
         return(obs)
+        #
+
+
+
+
+
+
+
+class PT_cell_theory(Theory):
+    """A class to return a PT C_ell module."""
+    # From yaml file.
+    dndzfn: str
+    #
+    def initialize(self):
+        """Sets up the class."""
+        self.dndz = np.loadtxt(self.dndzfn)
+        self.zmin = np.min(self.dndz[:,0])
+        self.zmax = np.max(self.dndz[:,0])
+    def get_requirements(self):
+        """What we need in order to provide C_ell."""
+        zg  = np.linspace(self.zmin,self.zmax,11,endpoint=True)
+        req = {\
+               'Pk_interpolator': {'k_max': 30,'z': zg,\
+                                   'nonlinear': False},\
+               'Hubble': {'z': [0]},\
+               'omegam': None\
+              }
+        return(req)
+    def get_can_provide(self):
+        """What do we provide: a PT class that can compute xi_ell."""
+        return ['pt_cell_mod']
+    def calculate(self, state, want_derived=True, **params_values_dict):
+        """Create and initialize the PT class."""
+        # Make shorter names and get params.
+        pp  = self.provider
+        OmM = pp.get_param('omegam')
+        hub = self.provider.get_Hubble(0)[0]/100.
+        # Set up the APS, including its zeff.
+        aps = T.AngularPowerSpectra(OmM,self.dndz)
+        # Get Plin.
+        ki  = np.logspace(-3.0,1.5,750)
+        pi  = pp.get_Pk_interpolator(nonlinear=False)
+        pi  = pi.P(aps.zeff,ki*hub)*hub**3
+        # and set the power spectrum module in APS:
+        aps.set_pk(ki,pi)
+        # Save the PT model in the state.
+        state['pt_cell_mod'] = aps
         #
